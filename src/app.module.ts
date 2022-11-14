e942d3adc72f20bf2cd26010cebda39f5657c199
Joey Yu <joey@itsjoeoui.com>
2022-11-14 15:41:11 -0500

    crud
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ItemsModule } from './items/items.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import keys from './config/keys';
 @Module({
-  imports: [],
+  imports: [ItemsModule, MongooseModule.forRoot(keys.mongoURL)],
   controllers: [AppController],
   providers: [AppService],
 })
